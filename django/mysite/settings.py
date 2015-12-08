8102357d9f384c53effff0ed8adafb8e90148917
asy11@duke.edu <asy11@duke.edu>
2015-12-07 21:05:43 -0500

    Annie got the changes

--- django/mysite/settings.py
+++ django/mysite/settings.py
@@ -65,7 +65,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': PROJ_NAME,                                  # Or path to database file if using sqlite3.
-        'USER': 'vagrant',                # Not used with sqlite3.
+        'USER': 'annie',                # Not used with sqlite3.
         'PASSWORD': 'dbpasswd',          # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
